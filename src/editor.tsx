11b6042c46813b609c3e9832e01158bce6c08180
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-05-28 15:47:38 +0700

    refactor : mass change + base feature

---
+++ src/editor.tsx
@@ -0,0 +1,33 @@
+import React, { useCallback, useEffect } from 'react'
+import useCodeMirror from './use-codemirror'
+import './editor.css';
+import { EditorState } from '@codemirror/state'
+
+interface Props {
+    initialDoc: string,
+    onChange: (doc: string) => void
+}
+
+const Editor: React.FC<Props> = (props) => {
+    const { onChange, initialDoc } = props
+    const handleChange = useCallback(
+        (state: EditorState) => onChange(state.doc.toString()),
+        [onChange]
+    )
+    const [refContainer, editorView] = useCodeMirror<HTMLDivElement>({
+        initialDoc: initialDoc,
+        onChange: handleChange
+    })
+
+    useEffect(() => {
+        if (editorView) {
+            // Do nothing for now
+        } else {
+            // loading editor
+        }
+    }, [editorView])
+
+    return <div className='editor-wrapper' ref={refContainer}></div>
+}
+
+export default Editor
---
+++ src/preview.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import remarkGfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import './preview.css'
+import 'github-markdown-css/github-markdown.css'
+import ReactMarkdown from 'react-markdown'
+
+interface Props {
+    doc: string
+}
+
+const Preview: React.FC<Props> = (props) => {
+    return <div className='preview markdown-body'>
+        <ReactMarkdown
+            children={props.doc}
+            remarkPlugins={[remarkGfm]}
+            components={{
+                code({ node, inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || '')
+                    return !inline && match ? (
+                        <SyntaxHighlighter
+                            children={String(children).replace(/\n$/, '')}
+                            style={prism}
+                            language={match[1]}
+                            PreTag="div"
+                            {...props}
+                        />
+                    ) : (
+                        <code className={className} {...props}>
+                            {children}
+                        </code>
+                    )
+                }
+            }}
+        />
+    </div>
+}
+
+export default Preview
