0261f1696ec251cdba45a3be482f818f7c7f9cd9
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-05-29 13:33:44 +0700

    feat : add preview bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import React, { useState, useCallback } from 'react'
 import Editor from './editor'
 import Preview from './preview'
 import './app.css'
+import Prevpane from './components/prevpane'
+
+const backgroundStyle = {
+  width: "100vw",
+  minHeight: "100vh",
+  maxHeight: "100vh",
+}
 
 const App: React.FC = () => {
   const [doc, setDoc] = useState<string>('# Hello, World!\n')
@@ -11,9 +18,12 @@ const App: React.FC = () => {
   }, [])
 
   return (
-    <div className="app">
-      <Editor onChange={handleDocChange} initialDoc={doc} />
-      <Preview doc={doc} />
+    <div style={backgroundStyle}>
+      <Prevpane />
+      <div className="app">
+        <Editor onChange={handleDocChange} initialDoc={doc} />
+        <Preview doc={doc} />
+      </div>
     </div>
   )
 }
---
+++ src/components/prevpane.tsx
@@ -0,0 +1,22 @@
+import React, { useCallback, useEffect } from 'react';
+import "./prevpane.css"
+
+const Prevpane = () => {
+    return (
+        <div className="main-prevpane">
+            <div className='info'>
+
+            </div>
+            <div className='label'>
+                <div className='label1'>
+                    Markdown
+                </div>
+                <div className='label2'>
+                    Preview
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Prevpane;
